f2d253bca44478191d77011b7579f2368dc258f0
Rob Hird <robertjhird@gmail.com>
2024-09-18 17:48:45 -0400

    main - initial work to integrate with open library

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	"github.com/celestialdragonfly/betterreads-platform/internal/data"
-	"github.com/celestialdragonfly/betterreads-platform/internal/dependency/google"
+	"github.com/celestialdragonfly/betterreads-platform/internal/dependency/openlibrary"
 	"github.com/celestialdragonfly/betterreads-platform/internal/package/env"
 	"github.com/celestialdragonfly/betterreads-platform/internal/package/log"
 	"github.com/celestialdragonfly/betterreads-platform/internal/server"
@@ -28,21 +28,12 @@ func run(ctx context.Context, w io.Writer, args []string) error {
 		port        = env.Int("HTTP_PORT", 4000, "API server port")
 		environment = env.String("DEPLOYMENT_ENV", "development", "Environment (development|staging|production)")
 
-		// Google API Envs
-		googleBookAPIKey = env.String("GOOGLE_BOOK_API_KEY", "", "API Key for the Google Books dependency.")
-
 		// Database Envs
-		databaseDSN                = env.String("DATABASE_DSN", "", "PostgreSQL DSN") // split this out into database name + password envs.
+		databaseDSN                = env.String("DATABASE_DSN", "postgres://betterreads:<>@localhost/betterreads", "PostgreSQL DSN") // split this out into database name + password envs.
 		databaseTimeout            = env.Duration("DATABASE_TIMEOUT", 5*time.Second, "Create a context with a 5-second timeout deadline.")
 		databaseMaxOpenConnections = env.Int("DATABASE_MAX_OPEN_CONNECTIONS", 25, "PostgreSQL max open connections")
 		databaseMaxIdleConnections = env.Int("DATABASE_MAX_IDLE_CONNECTIONS", 25, "PostgreSQL max idle connections")
 		databaseMaxIdleTime        = env.Duration("DATABASE_MAX_IDLE_CONNECTIONS", 15*time.Minute, "PostgreSQL max connection idle time")
-
-		cfg = server.Config{
-			Port:    port,
-			Env:     environment,
-			Version: version,
-		}
 	)
 
 	database, err := data.NewDatabase(&data.DBConfig{
@@ -55,17 +46,23 @@ func run(ctx context.Context, w io.Writer, args []string) error {
 	if err != nil {
 		panic(fmt.Errorf("error connecting to database %w", err))
 	}
-	googleBooksAPI := google.NewAPI(googleBookAPIKey)
 
 	logger := log.NewLogger(w)
-	app := server.NewBetterReads(logger, database, googleBooksAPI, &cfg)
+	app := server.NewBetterReads(&server.Config{
+		Port:           port,
+		Env:            environment,
+		Version:        version,
+		DB:             database,
+		Logger:         logger,
+		OpenLibraryAPI: openlibrary.NewAPI(logger),
+	})
 	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		Addr:         fmt.Sprintf(":%d", port),
 		Handler:      app.Handler,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		ErrorLog:     logger.NewErrorLogger(),
+		ErrorLog:     app.Logger.NewErrorLogger(),
 	}
 
 	go func() {
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,35 +2,44 @@ package server
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/celestialdragonfly/betterreads-platform/internal/contracts"
 	"github.com/celestialdragonfly/betterreads-platform/internal/data"
-	"github.com/celestialdragonfly/betterreads-platform/internal/dependency/google"
+	"github.com/celestialdragonfly/betterreads-platform/internal/dependency/openlibrary"
 	"github.com/celestialdragonfly/betterreads-platform/internal/package/json"
 	"github.com/celestialdragonfly/betterreads-platform/internal/package/log"
 	"github.com/julienschmidt/httprouter"
 )
 
 type Config struct {
-	Port    int
-	Env     string
-	Version string
+	Port           int
+	Env            string
+	Version        string
+	Handler        http.Handler
+	Logger         *log.Logger
+	DB             *data.SQL
+	OpenLibraryAPI *openlibrary.API
 }
 
 type BetterReads struct {
-	config        *Config
-	logger        *log.Logger
-	DB            *data.SQL
-	Handler       http.Handler
-	GoogleBookAPI *google.BookAPI
+	Port           int
+	Env            string
+	Version        string
+	Logger         *log.Logger
+	DB             *data.SQL
+	Handler        http.Handler
+	OpenLibraryAPI *openlibrary.API
 }
 
-func NewBetterReads(l *log.Logger, database *data.SQL, bookAPI *google.BookAPI, cfg *Config) *BetterReads {
+func NewBetterReads(cfg *Config) *BetterReads {
 	br := &BetterReads{
-		logger:        l,
-		DB:            database,
-		GoogleBookAPI: bookAPI,
-		config:        cfg,
+		Port:           cfg.Port,
+		Env:            cfg.Env,
+		Version:        cfg.Version,
+		Logger:         cfg.Logger,
+		DB:             cfg.DB,
+		OpenLibraryAPI: cfg.OpenLibraryAPI,
 	}
 	br.Handler = routes(br)
 	return br
@@ -47,7 +56,7 @@ func routes(br *BetterReads) http.Handler {
 	router.HandlerFunc(http.MethodGet, "/api/v1/users/:userid", br.GetUserHandler)
 
 	// Book Management
-	router.HandlerFunc(http.MethodGet, "/api/v1/books/search", br.GetGoogleBooks)                              // Query google books api for all books matching search param.
+	router.HandlerFunc(http.MethodGet, "/api/v1/books/search", br.SearchBooks)                                 // Query remote (OpenLibrary) books api for all books matching search param.
 	router.HandlerFunc(http.MethodGet, "/api/v1/books/library", br.GetBooksForUser)                            // Get all books from a user's library.
 	router.HandlerFunc(http.MethodGet, "/api/v1/books/library/:bookid", br.GetBookForUser)                     // Retrieves a signle book the user's library given the book id.
 	router.HandlerFunc(http.MethodDelete, "/api/v1/books/library/:bookid", br.RemoveBookForUser)               // Remove a book from a user's library given the book id.
@@ -70,38 +79,46 @@ func (br *BetterReads) GetUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // BOOKS-API
-// GetGoogleBooks query google books api for all books matching search param.
-func (br *BetterReads) GetGoogleBooks(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	wildcard := query.Get("q")
+// Search query OpenLibrary books api for all books matching search param.
+func (br *BetterReads) SearchBooks(w http.ResponseWriter, r *http.Request) {
+	rawQuery := r.URL.Query().Get("query")
+	rawPage := r.URL.Query().Get("page")
+	intPage := 0
+	if rawPage != "" {
+		var err error
+		intPage, err = strconv.Atoi(rawPage)
+		if err != nil {
+			json.Response(w, http.StatusPreconditionFailed, contracts.NewErrorResponse("invalid request: page must be an integer"), nil)
+		}
+	}
 
-	booksResponse, err := br.GoogleBookAPI.GetBooks(br.logger, wildcard)
+	_, err := br.OpenLibraryAPI.SearchBooks(rawQuery, openlibrary.DefaultBookFieldMask, intPage)
 	if err != nil {
 		json.Response(w, http.StatusInternalServerError, contracts.NewErrorResponse(err.Error()), nil)
 	}
 
-	books := make([]contracts.GoogleBook, 0)
-	for _, item := range booksResponse.Items {
-		book := contracts.GoogleBook{
-			ID:             item.ID,
-			Etag:           item.Etag,
-			SelfLink:       item.SelfLink,
-			Title:          item.VolumeInfo.Title,
-			Subtitle:       item.VolumeInfo.Subtitle,
-			Authors:        item.VolumeInfo.Authors,
-			Categories:     item.VolumeInfo.Categories,
-			AverageRating:  item.VolumeInfo.AverageRating,
-			RatingsCount:   item.VolumeInfo.RatingsCount,
-			MaturityRating: item.VolumeInfo.MaturityRating,
-			SmallThumbnail: item.VolumeInfo.ImageLinks.SmallThumbnail,
-			Thumbnail:      item.VolumeInfo.ImageLinks.Thumbnail,
-			PreviewLink:    item.VolumeInfo.PreviewLink,
-			InfoLink:       item.VolumeInfo.InfoLink,
-		}
-		books = append(books, book)
-	}
+	// books := make([]contracts.GoogleBook, 0)
+	// for _, item := range booksResponse.Items {
+	// 	book := contracts.GoogleBook{
+	// 		ID:             item.ID,
+	// 		Etag:           item.Etag,
+	// 		SelfLink:       item.SelfLink,
+	// 		Title:          item.VolumeInfo.Title,
+	// 		Subtitle:       item.VolumeInfo.Subtitle,
+	// 		Authors:        item.VolumeInfo.Authors,
+	// 		Categories:     item.VolumeInfo.Categories,
+	// 		AverageRating:  item.VolumeInfo.AverageRating,
+	// 		RatingsCount:   item.VolumeInfo.RatingsCount,
+	// 		MaturityRating: item.VolumeInfo.MaturityRating,
+	// 		SmallThumbnail: item.VolumeInfo.ImageLinks.SmallThumbnail,
+	// 		Thumbnail:      item.VolumeInfo.ImageLinks.Thumbnail,
+	// 		PreviewLink:    item.VolumeInfo.PreviewLink,
+	// 		InfoLink:       item.VolumeInfo.InfoLink,
+	// 	}
+	// 	books = append(books, book)
+	// }
 
-	json.Response(w, http.StatusOK, contracts.GetBooksResponse{Books: books}, nil)
+	// json.Response(w, http.StatusOK, contracts.GetBooksResponse{Books: books}, nil)
 }
 
 // GetBooksForUser retrieves all books from a user's library.
@@ -141,8 +158,8 @@ func (br *BetterReads) AddBookToWishlistForUser(w http.ResponseWriter, r *http.R
 func (br *BetterReads) HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	resp := contracts.HealthCheckResponse{
 		Status:      "available",
-		Environment: br.config.Env,
-		Version:     br.config.Version,
+		Environment: br.Env,
+		Version:     br.Version,
 	}
 
 	json.Response(w, http.StatusOK, resp, nil)
